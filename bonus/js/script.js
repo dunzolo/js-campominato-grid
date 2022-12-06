7345839a4c3217f9385ee95629d10452a388f5db
dunzolo <rossidavide108@gmail.com>
2022-12-06 16:47:25 +0100

    fixed text into square

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,10 +1,12 @@
 // inizio funzioni
 
 //creo il tag div con relativa classe
-function generateSquare(){
+function generateSquare(num){
     const element = document.createElement('div');
 
     element.classList.add('square');
+
+    element.innerText = num;
 
     return element;
 }
@@ -19,23 +21,24 @@ let difficulty = document.getElementById('difficulty');
 
 button.addEventListener('click', function(){
 
-    if(grid)
+    //se è presente la griglia, la cancella
+    if(grid){
         grid.innerHTML="";
+    }
 
     if(difficulty[0].selected == true){
         // creo un ciclo for per aggiungere le caselle all'interno della griglia
         for(let i = 1; i <= 100; i++){
         
             // assegno alla variabile, l'elemento generato dalla funzione
-            let square = generateSquare();
+            let square = generateSquare(i);
         
             square.classList.add('square-100');
 
             square.addEventListener('click',function(){
                 this.classList.add('checked');
                 
-                this.innerText = i;
-                alert(`La casella selezionata contiene il numero: ${this.innerText}`);
+                console.log(`La casella selezionata contiene il numero: ${i}`);
             });
             
             // aggiungo gli elementi alla griglia in HTML
@@ -47,15 +50,14 @@ button.addEventListener('click', function(){
         for(let i = 1; i <= 81; i++){
         
             // assegno alla variabile, l'elemento generato dalla funzione
-            let square = generateSquare();
+            let square = generateSquare(i);
         
             square.classList.add('square-81');
 
             square.addEventListener('click',function(){
                 this.classList.add('checked');
                 
-                this.innerText = i;
-                alert(`La casella selezionata contiene il numero: ${this.innerText}`);
+                console.log(`La casella selezionata contiene il numero: ${i}`);
             });
             
             // aggiungo gli elementi alla griglia in HTML
@@ -67,15 +69,14 @@ button.addEventListener('click', function(){
         for(let i = 1; i <= 49; i++){
         
             // assegno alla variabile, l'elemento generato dalla funzione
-            let square = generateSquare();
+            let square = generateSquare(i);
         
             square.classList.add('square-49');
 
             square.addEventListener('click',function(){
                 this.classList.add('checked');
                 
-                this.innerText = i;
-                alert(`La casella selezionata contiene il numero: ${this.innerText}`);
+                console.log(`La casella selezionata contiene il numero: ${i}`);
             });
             
             // aggiungo gli elementi alla griglia in HTML
