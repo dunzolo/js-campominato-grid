8f4000796fd8c6b33e422613d419458699fef082
dunzolo <rossidavide108@gmail.com>
2022-12-06 16:49:42 +0100

    fixed text into square

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 // inizio funzioni
 
 //creo il tag div con relativa classe
-function generateSquare(){
+function generateSquare(num){
     const element = document.createElement('div');
 
     element.classList.add('square');
+
+    element.innerText = num;
 
     return element;
 }
@@ -18,17 +20,21 @@ let button = document.getElementById('play');
 
 button.addEventListener('click', function(){
     
+    //se è presente una griglia, la canella
+    if(grid){
+        grid.innerHTML="";
+    }
+
     // creo un ciclo for per aggiungere le caselle all'interno della griglia
     for(let i = 1; i <= 100; i++){
     
         // assegno alla variabile, l'elemento generato dalla funzione
-        let square = generateSquare();
+        let square = generateSquare(i);
     
         square.addEventListener('click',function(){
             this.classList.add('checked');
             
-            this.innerText = i;
-            alert(`La casella selezionata contiene il numero: ${this.innerText}`);
+            console.log(`La casella selezionata contiene il numero: ${i}`);
         });
         
         // aggiungo gli elementi alla griglia in HTML
