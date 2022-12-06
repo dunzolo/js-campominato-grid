fd98412f77ef6b3376bc95394606ea82ec39e181
dunzolo <rossidavide108@gmail.com>
2022-12-06 15:27:08 +0100

    added button play

--- js/script.js
+++ js/script.js
@@ -11,22 +11,28 @@ function generateSquare(){
 
 //fine funzioni
 
-
 let grid = document.getElementById('grid');
 
-// creo un ciclo for per aggiungere le caselle all'interno della griglia
-for(let i = 1; i <= 100; i++){
+let button = document.getElementById('play');
 
-    // assegno alla variabile, l'elemento generato dalla funzione
-    let square = generateSquare();
 
-    square.addEventListener('click',function(){
-        this.classList.add('checked');
-        
-        this.innerText = i;
-        console.log(this.innerText);
-    });
+button.addEventListener('click', function(){
     
-    // aggiungo gli elementi alla griglia in HTML
-    grid.appendChild(square);
-}
+    // creo un ciclo for per aggiungere le caselle all'interno della griglia
+    for(let i = 1; i <= 100; i++){
+    
+        // assegno alla variabile, l'elemento generato dalla funzione
+        let square = generateSquare();
+    
+        square.addEventListener('click',function(){
+            this.classList.add('checked');
+            
+            this.innerText = i;
+            alert(`La casella selezionata contiene il numero: ${this.innerText}`);
+        });
+        
+        // aggiungo gli elementi alla griglia in HTML
+        grid.appendChild(square);
+    }
+});
+
