3f7a611b340038adbe0655e2084a7a2d2164e2c6
dunzolo <rossidavide108@gmail.com>
2022-12-06 15:07:26 +0100

    added text to square

--- js/script.js
+++ js/script.js
@@ -19,6 +19,13 @@ for(let i = 1; i <= 100; i++){
 
     // assegno alla variabile, l'elemento generato dalla funzione
     let square = generateSquare();
+
+    square.addEventListener('click',function(){
+        this.classList.add('checked');
+        
+        this.innerText = i;
+        console.log(this.innerText);
+    });
     
     // aggiungo gli elementi alla griglia in HTML
     grid.appendChild(square);
