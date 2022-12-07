266c47ada11baa6bd0445006fdcb884e56c00bca
dunzolo <rossidavide108@gmail.com>
2022-12-07 10:18:54 +0100

    added style

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 // inizio funzioni
 
 //creo il tag div con relativa classe
-function generateSquare(num){
+function generateSquare(){
     const element = document.createElement('div');
 
     element.classList.add('square');
-
-    element.innerText = num;
 
     return element;
 }
@@ -26,15 +24,17 @@ button.addEventListener('click', function(){
     }
 
     // creo un ciclo for per aggiungere le caselle all'interno della griglia
-    for(let i = 1; i <= 100; i++){
+    for(let i = 0; i < 100; i++){
     
         // assegno alla variabile, l'elemento generato dalla funzione
         let square = generateSquare(i);
-    
+        
+        square.innerText = i + 1;
+
         square.addEventListener('click',function(){
             this.classList.add('checked');
-            
-            console.log(`La casella selezionata contiene il numero: ${i}`);
+
+            console.log(`La casella selezionata contiene il numero: ${this.innerText}`);
         });
         
         // aggiungo gli elementi alla griglia in HTML
